da568c4bcc88d6268bf052680175ace6daf8587b
Michał Kowalik <kowalik.michall@users.noreply.github.com>
2018-07-25 17:22:11 +0200

    add settings tab

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+let TextContent= {'#home': "🎮 Let's start", '#quizsettings': "👌 Look's good", "#quizstart": '▶ Next question', '#quizend': "Go to high scores", '#scores': '🔁 Play again'};
+
 $(function(){
-	$('.tabText').on('click', '.next', ()=> NextTabs());
-	$('.tabText').on('click', '.previous', ()=> PreviousTabs());
+	$('.row-button').on('click', '.next', ()=> NextTabs());
+	$('.row-button').on('click', '.previous', ()=> PreviousTabs());
+	$('.nav-tabs').on('click', ()=> ChangeButtonText($('.nav-tabs li.active').children().attr('href')));
+	ChangeButtonText('#home');
 });
 
 function NextTabs(){
@@ -28,3 +32,24 @@ function ChangeTab(element){
 	element.addClass('active');
 	element.find('a[data-toggle="tab"]').click();
 }
+
+function ChangeButtonText(element){
+	console.log(element);
+	$('.buttonTextContent').text(TextContent[element]);
+}
+
+// $.ajax({
+// 	url: 'https://opentdb.com/api.php?amount=10&category=10&difficulty=easy',
+// 	type: 'GET',
+// 	dataType: 'JSON',
+// })
+// .done(function(data) {
+// 	console.log("success");
+// 	console.log(data);
+// })
+// .fail(function() {
+// 	console.log("error");
+// });
+
+
+//https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple
