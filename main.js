7238acf0a338d49488416b271dd6655ded9bc65d
Michał Kowalik <kowalik.michall@users.noreply.github.com>
2018-07-25 11:49:37 +0200

    Change layout

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 $(function(){
-	$('.row-button').on('click', '.next', ()=> NextTabs());
+	$('.tabText').on('click', '.next', ()=> NextTabs());
+	$('.tabText').on('click', '.previous', ()=> PreviousTabs());
 });
 
 function NextTabs(){
 	var activeTab = $('.nav-tabs li.active');
-		activeTab.removeClass('active');
-		activeTab.find('a').addClass('disabled');
-		activeTab.find('button').addClass('disabled');
-		activeTab.find('button').css('border-style', 'solid')
-	var nexTab = activeTab.next();
-		nexTab.find('a').removeClass('disabled');
-		nexTab.find('button').removeClass('disabled');
-		nexTab.addClass('active');
-		nexTab.find('a[data-toggle="tab"]').click();
+	(activeTab.next().length !== 1) ? null:CurrntTabs(activeTab);
+	ChangeTab(activeTab.next());
 }
 
+function PreviousTabs(){
+	var activeTab = $('.nav-tabs li.active');
+	(activeTab.prev().length !== 1) ? null:CurrntTabs(activeTab);
+	ChangeTab(activeTab.prev());
+}
 
+function CurrntTabs(activeTab){
+	activeTab.removeClass('active');
+	activeTab.find('a').addClass('disabled');
+	activeTab.find('button').addClass('disabled');
+	activeTab.find('button').css('border-style', 'solid');
+}
+
+function ChangeTab(element){
+	element.find('a').removeClass('disabled');
+	element.find('button').removeClass('disabled');
+	element.addClass('active');
+	element.find('a[data-toggle="tab"]').click();
+}
